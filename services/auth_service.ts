44f1ba8e5935fafea120d584b695b9385a95858e
Louay Badri <louay.badri@insat.ucar.tn>
2023-01-29 14:41:40 +0100

    registration service done

--- services/auth_service.ts
+++ services/auth_service.ts
@@ -1,8 +1,18 @@
 import { UserData } from "../types";
+import { getAuth, createUserWithEmailAndPassword , deleteUser } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore"; 
+import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import {firebaseConfig} from "../config/firebase"
+import "../consts/firebase";
+import { USERS } from "../consts/firebase";
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
 
 export async function googleLogin() : Promise<UserData> {
     const template: UserData = {
-        name: "",
+        name: "",   
         email: "",
     }
     return template;
@@ -16,10 +26,30 @@ export async function emailLogin(email: String, password: String) : Promise<User
     return template;
 }
 
-export async function register(userData: UserData, password: String) : Promise<UserData> {
+export async function register(
+    userData: UserData,
+    password: String
+): Promise<UserData> {
     const template: UserData = {
-        name: "",
-        email: "",
+        name: userData["name"],
+        email: userData["email"],
+    };
+
+    const auth = getAuth();
+   
+    const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        template.email.toString(),
+        password.toString()
+    )
+    try {
+        
+    await setDoc(doc(db, USERS ,userCredential.user.uid),template)
+    } catch (error) {
+         
+        deleteUser(userCredential.user)
+        throw error
     }
+
     return template;
 }
