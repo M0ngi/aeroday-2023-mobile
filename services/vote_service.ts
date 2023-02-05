230446c427004324648286dc8f1549c1638e43d0
Firas Mosbahi <firasmosbahi10@gmail.com>
2023-02-05 17:16:16 +0100

    air_show_function

--- services/vote_service.ts
+++ services/vote_service.ts
@@ -1,28 +1,23 @@
 import {firebaseConfig} from "../config/firebase"
 import {USERS,AIRSHOW} from "../consts/firebase"
 import { initializeApp } from "firebase/app"
-import { getFirestore,updateDoc,doc, getDoc } from "firebase/firestore"
+import { getFirestore,updateDoc,doc } from "firebase/firestore"
+import firebase from "firebase/compat";
 
 export async function airshowVote(userId: string, teamId: string) : Promise<boolean>{
 
     const app = initializeApp(firebaseConfig);
     const db = getFirestore(app);
     const userDocRef = doc(db, USERS, userId);
-    const userDocSnap = await getDoc(userDocRef);
-
-    if (userDocSnap.exists()) {
-        if(userDocSnap.get("AirShowVote") != null){
-            const airShowDocRef = doc(db, AIRSHOW, teamId);
-            const airShowDocSnap = await getDoc(userDocRef);
-            const votes:number = airShowDocSnap.get("Votes");
-            await updateDoc(airShowDocRef,{"Votes" : votes+1});
-            await updateDoc(userDocRef, {"AirShowVote" : teamId})
-            return true;
+    const airShowDocRef = firebase.database().ref(AIRSHOW+"/"+teamId);
+    await airShowDocRef.transaction(function (doc) {
+        if (doc) {
+            doc.Votes = (doc.Votes || 0) + 1;
         }
-        else throw new Error("No such vote");
-    } else {
-        throw new Error("The user has already voted, he can vote once");
-    }
+        return doc;
+    });
+    await updateDoc(userDocRef, {"AirShowVote" : teamId})
+    return true;
 }
 
 export async function videoGraphieVote(userId: String, teamId: String) : Promise<boolean>{
