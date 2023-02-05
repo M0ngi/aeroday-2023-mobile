1f9f5ffc83721f7e6981ccc0cdfc8e60f9a77fa8
Firas Mosbahi <firasmosbahi10@gmail.com>
2023-02-05 20:54:19 +0100

    air_show_function

--- services/vote_service.ts
+++ services/vote_service.ts
@@ -1,22 +1,27 @@
 import {firebaseConfig} from "../config/firebase"
 import {USERS,AIRSHOW} from "../consts/firebase"
 import { initializeApp } from "firebase/app"
-import { getFirestore,updateDoc,doc } from "firebase/firestore"
+import { getFirestore,updateDoc,doc,getDoc } from "firebase/firestore"
+import { getDatabase, ref, runTransaction } from "firebase/database";
 import firebase from "firebase/compat";
 
 export async function airshowVote(userId: string, teamId: string) : Promise<boolean>{
-
     const app = initializeApp(firebaseConfig);
     const db = getFirestore(app);
     const userDocRef = doc(db, USERS, userId);
-    const airShowDocRef = firebase.database().ref(AIRSHOW+"/"+teamId);
-    await airShowDocRef.transaction(function (doc) {
-        if (doc) {
-            doc.Votes = (doc.Votes || 0) + 1;
+    const userDoc = await getDoc(userDocRef);
+    const oldVote: string | null = userDoc.data().Vote;
+    await updateDoc(userDocRef, {"AirShowVote" : teamId});
+    try {
+        const updates = {};
+        updates[`${AIRSHOW}/${teamId}/Votes`] = firebase.database.ServerValue.increment(1);
+        if (oldVote != null) {
+            updates[`${AIRSHOW}/${oldVote}/Votes`] = firebase.database.ServerValue.increment(-1);
         }
-        return doc;
-    });
-    await updateDoc(userDocRef, {"AirShowVote" : teamId})
+        await firebase.database().ref().update(updates);
+    }catch (e) {
+        await updateDoc(userDocRef, {"AirShowVote" : oldVote});
+    }
     return true;
 }
 
