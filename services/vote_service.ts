0791d8f1a89960ebd96e9d5e18c78b52589b2140
Firas Mosbahi <firasmosbahi10@gmail.com>
2023-02-07 18:48:30 +0100

    air_show_function

--- services/vote_service.ts
+++ services/vote_service.ts
@@ -1,27 +1,23 @@
-import {firebaseConfig} from "../config/firebase"
 import {USERS,AIRSHOW} from "../consts/firebase"
-import { initializeApp } from "firebase/app"
-import { getFirestore,updateDoc,doc,getDoc } from "firebase/firestore"
-import { getDatabase, ref, runTransaction } from "firebase/database";
+import { getDatabase, ref ,get ,child } from "firebase/database";
 import firebase from "firebase/compat";
 
 export async function airshowVote(userId: string, teamId: string) : Promise<boolean>{
-    const app = initializeApp(firebaseConfig);
-    const db = getFirestore(app);
-    const userDocRef = doc(db, USERS, userId);
-    const userDoc = await getDoc(userDocRef);
-    const oldVote: string | null = userDoc.data().Vote;
-    await updateDoc(userDocRef, {"AirShowVote" : teamId});
-    try {
-        const updates = {};
-        updates[`${AIRSHOW}/${teamId}/Votes`] = firebase.database.ServerValue.increment(1);
-        if (oldVote != null) {
-            updates[`${AIRSHOW}/${oldVote}/Votes`] = firebase.database.ServerValue.increment(-1);
+    let oldVote: string | null = null;
+    get(child(ref(getDatabase()), `${USERS}/${userId}/Vote`)).then((snapshot) => {
+        if (snapshot.exists()) {
+            oldVote = snapshot.val();
         }
-        await firebase.database().ref().update(updates);
-    }catch (e) {
-        await updateDoc(userDocRef, {"AirShowVote" : oldVote});
+    }).catch((error) => {
+        console.error(error);
+    });
+    const updates = {};
+    updates[`${USERS}/${userId}/Vote}`] = teamId;
+    updates[`${AIRSHOW}/${teamId}/Votes`] = firebase.database.ServerValue.increment(1);
+    if (oldVote != null) {
+        updates[`${AIRSHOW}/${oldVote}/Votes`] = firebase.database.ServerValue.increment(-1);
     }
+    await firebase.database().ref().update(updates);
     return true;
 }
 
