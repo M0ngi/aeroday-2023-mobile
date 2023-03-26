3495f8d5e337c8583fd79c5eb8d81a3f83970f22
M0ngi <saidanemongi@gmail.com>
2023-03-26 03:13:18 +0100

    Remove unused files

--- services/participants_service.ts
+++
@@ -1,9 +0,0 @@
-import { AirshowTeam, VideographieTeam } from "../types";
-
-export async function getAirshowTeams() : Promise<Array<AirshowTeam>>{
-    return [];
-}
-
-export async function getVideoGraphieTeams() : Promise<Array<VideographieTeam>>{
-    return [];
-}
--- services/vote_service.ts
+++
@@ -1,8 +0,0 @@
-export async function airshowVote(userId: String, teamId: String) : Promise<boolean>{
-    return false;
-}
-
-export async function videoGraphieVote(userId: String, teamId: String) : Promise<boolean>{
-    return false;
-}
-
